6729a7417b4bbb794701b6b69b6ebd7a2d36e3de
psypy1 <psypy1@nottingham.ac.uk>
2018-12-10 15:23:58 +0000

    1. now when click sign in in plug-in, it could directly jump to task page, though I don't know why

--- plug-in/webrake/js/popup.js
+++ plug-in/webrake/js/popup.js
@@ -7,15 +7,23 @@ window.onload = function login() {
 	// window.location.href = "../html/ourweb.html";
 	    var username = document.getElementById("username");
 	    var pass = document.getElementById("password");
-	 
-	    if (username.value == "") {
-	        alert("Please enter your username");
-	    } else if (pass.value  == "") {
-	        alert("Please enter your password");
-	    } else if(username.value == "psyad10" && pass.value == "123456"){
-			chrome.browserAction.setPopup({popup: "../html/ourweb.html"});
-	    } else {
-	        alert("Please enter the correct user name or password! ")
-	    }
-		});
+	    	let params = {
+		active: true,
+		currentWindow: true
+		}
+		chrome.tabs.query(params, getTabs);
+		
+		function getTabs(tab){
+			if (username.value == "") {
+        			alert("Please enter your username");
+    			} else if (pass.value  == "") {
+        			alert("Please enter your password");
+    			} else if(username.value == "psyad10" && pass.value == "123456"){
+    				window.location.href ="../html/ourweb.html"
+				chrome.browserAction.setPopup({tabId:tab[0].id, popup: "../html/ourweb.html"});
+    			} else {
+        			alert("Please enter the correct user name or password! ")
+    			}
+		}
+	})
 }
