08f0c3af4ef29299fb17dc9eb881ad475049c83a
psypy1 <psypy1@nottingham.ac.uk>
2019-01-04 17:39:17 +0800

    1. add refinitive logo 2. when click add task in main, content script could workon all tabs after flashing the page.

--- plug-in/webrake/js/popup.js
+++ plug-in/webrake/js/popup.js
@@ -1,15 +1,10 @@
-// document.addEventListener('DOMContentLoaded', function(){
-// 	document.getElementById("btn_login").addEventListener('click',login());
-// });
-// 
 window.onload = function login() {
 	document.getElementById("btn_login").addEventListener('click', function(){
-	// window.location.href = "../html/ourweb.html";
 	    var username = document.getElementById("username");
 	    var pass = document.getElementById("password");
 	    	let params = {
-		active: true,
-		currentWindow: true
+			active: true,
+			currentWindow: true
 		}
 		chrome.tabs.query(params, getTabs);
 		
@@ -19,10 +14,9 @@ window.onload = function login() {
     			} else if (pass.value  == "") {
         			alert("Please enter your password");
     			} else if(username.value == "psyad10" && pass.value == "123456"){
-    				window.location.href ="../html/ourweb.html"
-				chrome.browserAction.setPopup({tabId:tab[0].id, popup: "../html/ourweb.html"});
+    				window.location.href ="../html/main.html";
     			} else {
-        			alert("Please enter the correct user name or password! ")
+        			alert("Please enter the correct user name or password! ");
     			}
 		}
 	})
