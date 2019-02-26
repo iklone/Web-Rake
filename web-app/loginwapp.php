6041aa4b77a0d4344bf844ea747bcaa459396893
psyjct <psyjct@nottingham.ac.uk>
2019-02-26 12:30:49 +0000

    login implementation and session vars

---
+++ web-app/loginwapp.php
@@ -0,0 +1,18 @@
+<?php
+// Start the session
+session_start();
+?>
+<head>
+</head>
+<body>
+<?php
+include "login.php";
+if($pa == $password){
+	$_SESSION["userId"] = $userId[0];
+	$_SESSION["userName"] = $_POST["username"];
+	header('Location: mainseven.php');
+} else {
+	header('Location: index.html');
+}
+?>
+</body>
