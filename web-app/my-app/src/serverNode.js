7cfd2ebc5de1a0ffed49fb094cf34b9a94ae0958
psyoa3 <psyoa3@nottingham.ac.uk>
2019-03-25 11:37:11 +0000

    Create connection to MySQL database in Server Node

--- web-app/my-app/src/serverNode.js
+++ web-app/my-app/src/serverNode.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mysql = require('mysql');
+const SELECT_ALL_TASKS_QUERY = "SELECT * FROM Task";
 
 app.use(cors());
+app.get('/tasks', (req, res) => {
+    console.log("GO TO /tasks TO SEE ALL TASKS");
+
+    const conn = mysql.createConnection({
+        host: 'mysql.cs.nott.ac.uk',
+        user: 'psyjct',
+        password: '1234Fred',
+        database: 'psyjct'
+    });
+
+    conn.query(SELECT_ALL_TASKS_QUERY, (err,rows, fields) => {
+        if (err) {
+            return err;
+        }
+    })
+});
+
 app.listen(4000, () => {
     console.log("Server is up and listening on 4000...")
 });
-
-
-
-
