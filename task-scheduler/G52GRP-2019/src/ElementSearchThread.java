8ab28e233345fa7301dcaeed9a74939de2e630ea
psyhh1 <psyhh1@nottingham.ac.uk>
2019-03-26 13:05:12 +0000

    isDateTime and isStillDateTime added to check if element has changed

--- task-scheduler/G52GRP-2019/src/ElementSearchThread.java
+++ task-scheduler/G52GRP-2019/src/ElementSearchThread.java
@@ -1,10 +1,16 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
@@ -18,6 +24,8 @@ public class ElementSearchThread implements Runnable {
 	private String element;
 	private String elementID;
 	private int flag;
+	private String sampleData;
+	private String scrapeName;
 	private ScrapeResult result;
 	private String value;
 	private WebClient webClient;
@@ -33,6 +41,52 @@ public class ElementSearchThread implements Runnable {
 	
 	@Override
 	public void run() {
+	    try {
+			page = webClient.getPage(urlStr);
+		}
+	    catch (Exception e) {
+	    	System.out.println("Couldn't download the page for task: " + this.taskID);
+			Connection conn = null;
+			Statement stmt = null;
+			
+			try {
+				conn = ConnectionManager.getConnection();
+				stmt = conn.createStatement();
+			    
+			    String sql = "UPDATE Scrape SET flag = " + 4 + " WHERE TaskID = " + this.taskID;
+			    stmt.executeUpdate(sql);
+				System.out.println("Updated flag in Scrape table where taskID = " + this.taskID);
+				
+				stmt.close();
+				conn.close();
+			}
+			catch(SQLException se) {
+				//Handle errors for JDBC
+				se.printStackTrace();
+			}
+			catch(Exception e2) {
+				//Handle errors for Class.forName
+				e.printStackTrace();
+			}
+			finally {
+				//finally block used to close resources
+				try {
+					if(stmt != null)
+						stmt.close();
+				}
+				catch(SQLException se2) {
+					// nothing we can do
+				}
+				try {
+					if(conn != null)
+						conn.close();
+				}
+				catch(SQLException se) {
+					se.printStackTrace();
+				}//end finally try
+		   }
+			return;
+		}  
 		insertResultIntoDatabase();
 	   	webClient.close(); // plug memory leaks
 	}
@@ -71,6 +125,8 @@ public class ElementSearchThread implements Runnable {
 				element = rs.getString("Element");
 				elementID = rs.getString("elementID");
 				flag = rs.getInt("flag");
+				sampleData = rs.getString("sampleData");
+				scrapeName = rs.getString("scrapeName");
 				System.out.println("Scraping element: " + element + " from: " + this.urlStr);
 				
 				if (flag != 2)
@@ -174,22 +230,21 @@ public class ElementSearchThread implements Runnable {
 	public void getHTMLUnitResult(String element, String elementID) {
 		System.out.println("Getting result from " + urlStr);
 
-		if (!scrape() && (result.getFlag() != 4)) {
-			/*if (searchAI(element, elementID, page)) {
+		if (!scrape()) {
+			if (searchAI(element, elementID, page)) {
 				scrape(); // On AI success, scrape again
 				result.setFlag(1);
 			}
-			else {*/
+			else {
 				result.setFlag(2);
-			//}
+			}
 		}
 	}
 	
 	public Boolean scrape() {
 		Boolean foundElement = false;
 		
-		try {  
-		    page = webClient.getPage(urlStr);    
+		try {    
 			HtmlElement e = page.getFirstByXPath(element);
 			
 			if (e == null) {
@@ -197,11 +252,10 @@ public class ElementSearchThread implements Runnable {
 				int clickedPages = 0;
 				HtmlElement acceptButton = null;
 				Boolean acceptButtonFound = false;
-				System.out.println("Couldn't find element. Trying the buttons.");
+				System.out.println("Couldn't find element for scrape with ID: " + this.scrapeID + ". Trying the buttons.");
 				
 				List<HtmlElement> buttons = page.getByXPath("//button");
 				for (HtmlElement e2: buttons) {
-					System.out.println("GETS HERE Button text: " + e2.asText());
 					String[] acceptSynonyms = {"accept", "submit", "ok", "agree"};
 					for (i = 0; i < acceptSynonyms.length; i++) {
 						if (e2.asText().toLowerCase().equals(acceptSynonyms[i])) {
@@ -227,7 +281,6 @@ public class ElementSearchThread implements Runnable {
 			        
 					for (HtmlElement e2 : submitButtons) {
 						System.out.println("Button text: " + e2.asText());
-						// Can we check each button innerHTML for 'OK' or 'I accept' or 'Accept' or ... 'Agree' ?
 						page = e2.click();
 						if ((e = (HtmlElement) page.getFirstByXPath(element)) != null) {
 							break;
@@ -241,6 +294,9 @@ public class ElementSearchThread implements Runnable {
 			}
 			
 			if (e != null) {
+				// check the type and neighbours to ensure the element is still the same, if not return false
+				if (isStillNumeric(e.asText()) && isStillDateTime(e.asText()))
+					return foundElement;
 				result.setResult(e.getTextContent());
 				result.setFlag(0);
 				foundElement = true;
@@ -248,10 +304,109 @@ public class ElementSearchThread implements Runnable {
 		}
 		catch (Exception e) {
 			e.printStackTrace();
-			result.setFlag(4); // couldn't even download the webpage
+
 		}
 		
 		return foundElement;
+	}
+	
+	public Boolean isNumeric(String element) {
+		if (Pattern.matches("^(\\d+|\\d{1,3}(,\\d{3})*)(\\.\\d+)?$", element)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean isStillNumeric(String scrapedElement) {
+		Boolean hasChanged = false;
+		
+		// check type and neighbours
+		if (isNumeric(this.sampleData) ) { // number
+			if (!isNumeric(scrapedElement) ) {
+				hasChanged = true;
+			}
+		}
+
+		return hasChanged;
+	}
+	
+	public Boolean isDateTime(String element) {
+		if (Pattern.matches("[0-9]{2}[\\/\\-,.][0-9]{2}[\\/\\-,.](19|20)[0-9]{2}", element)) { //01/01/1998
+			return true;
+		}
+		else if (Pattern.matches("(19|20)[0-9]{2}[\\/\\-,.][0-9]{2}[\\/\\-,.][0-9]{2}", element)) { // 1998/01/01
+			return true;
+		}
+		else if (Pattern.matches("[0-9]{2}[\\/\\-,.][0-9]{2}[\\/\\-,.][0-9]{2}", element)) { // 01/01/98
+			return true;
+		}
+		else if (Pattern.matches("[0-9]{2}[\\/\\-,.][A-Z|a-z]{3}[\\/\\-,.][0-9]{2}", element)) { // 01-JAN-98
+			return true;
+		}
+		else if (Pattern.matches("[0-9]{2}[\\/\\-,.][A-Z|a-z]{3}[\\/\\-,.](19|20)[0-9]{2}", element)) { // 01-JAN-1998
+			return true;
+		}
+		else if (Pattern.matches("[0-9]{2}[\\/\\-,.][A-Z|a-z]{3}[\\/\\-,.](19|20)[0-9]{2} [0-2][0-9]:[0-5][0-9]", element)) { // 01-JAN-1998 08:59
+			return true;
+		}
+		else if (Pattern.matches("[0-9]{2}[\\/\\-,.][A-Z|a-z]{3}[\\/\\-,.](19|20)[0-9]{2} [0-2][0-9]:[0-5][0-9]:[0-5][0-9]", element)) { // 01-JAN-1998 08:59:00
+			return true;
+		} //START OF REGEXS MATCHING EXAMPLE WEBSITES
+		else if (Pattern.matches("[[0-3]{0,1}[0-9]([s|n|r|t][t|d|h]){0,1}[\\/\\-,. ][A-Z|a-z]{3}.{0,1}[\\/\\-,. ](19|20){0,1}[0-9][0-9]", element)) { // 1st Jan 2019
+			return true;
+		}
+		else if (Pattern.matches("[0-9]{2}[\\/\\-,.][0-9]{2}[\\/\\-,.](19|20)[0-9]{2}[ ]{0,1}[-]{1}[ ]{0,1}[0-9]{2}[\\/\\-,.][0-9]{2}[\\/\\-,.](19|20)[0-9]{2}", element)) { // 01/01/1999 - 01/01/1999
+			return true;
+		}
+		else if (Pattern.matches("[0-9]{2}[\\/\\-,.][A-Z|a-z]{3}[\\/\\-,.](19|20)[0-9]{2} [0-2][0-9]:[0-5][0-9]:[0-5][0-9].[0-9]*", element)) { // 19-Feb-2019 08:05:33.000000000000000
+			return true;
+		}
+		
+		
+		
+		
+		return false;
+	}
+	
+	public Boolean isStillDateTime(String scrapedElement) {
+		Boolean hasChanged = false;
+		
+		// check type and neighbours
+		if (isDateTime(this.sampleData) ) { // number
+			if (!isDateTime(scrapedElement) ) {
+				hasChanged = true;
+			}
+		}
+
+		return hasChanged;
+	}
+	
+	public Boolean isCurrency(String element) {
+		if (Pattern.matches("^(\\d+|\\d{1,3}(,\\d{3})*)(\\.\\d+)?$", element)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean isStillCurrency(String scrapedElement) {
+		Boolean hasChanged = false;
+		
+		// check type and neighbours
+		if (isCurrency(this.sampleData) ) { // number
+			if (!isCurrency(scrapedElement) ) {
+				hasChanged = true;
+			}
+		}
+
+		return hasChanged;
+	}
+	
+	public String findParent(String element) {
+		int index = element.lastIndexOf("/");
+		if (index != 0) {
+			return element.substring(0, index);
+		}
+		return element;
 	}
 	
 	public Boolean searchAI(String element, String elementID, HtmlPage page) {
@@ -259,10 +414,9 @@ public class ElementSearchThread implements Runnable {
 		Statement stmt = null;
 		
 		int depth = 0;
-		String[] parts = element.split("/");
-		parts = parts[parts.length].split("[");
-		String tag = parts[0];
-		HtmlElement htmlElement;
+		int index = 0;
+		int i;
+		boolean passed;
 		
 		try {
 			conn = ConnectionManager.getConnection();
@@ -307,21 +461,38 @@ public class ElementSearchThread implements Runnable {
 				se.printStackTrace();
 			}//end finally try
 	   }//end try
-		
-		for (int i = 0; i < depth; i++) {
-			// for every parent, completely evaluate whether any of their descendants are the element
-			// by checking their ids
-			element += "/..";
 
-			for (Object o : page.getByXPath(".//" + tag)) {
-				htmlElement = (HtmlElement) o;
-				if (htmlElement.getId() == elementID) {
-					updateXPath(htmlElement.getCanonicalXPath(), i);
-					return true;
+		System.out.println("AI init");
+		for (i = 0; i < depth + 1; i++) {
+			System.out.println("depth attempt " + i);
+			element = findParent(element);
+			System.out.println("parent  " + element);
+			HtmlElement parent = page.getFirstByXPath(element);
+			System.out.println("testing parent  " + parent.asText());
+			Iterable<DomElement> childList = parent.getChildElements();
+			
+
+			System.out.println("this has cousins " + parent.getChildElementCount());
+			for (DomElement e : childList) {
+
+				System.out.println("testing family " + e.asText());
+				System.out.println("!");
+				passed = false;
+				if (elementID != "") {
+					if (e.getId() == elementID && isNumeric(e.asText()) ) {
+						passed = true;
+					}
+				} else {
+					if (isNumeric(e.asText())) {
+						passed = true;
+					}
+				}
+				
+				if (passed) {
+					updateXPath(e.getCanonicalXPath(), i);
 				}
 			}
 		}
-		
 		return false;
 	}
 	
@@ -341,7 +512,7 @@ public class ElementSearchThread implements Runnable {
 			
 			sql = "INSERT INTO Intervention (scrapeID, Depth) VALUES (" + scrapeID + "," + depth + ")";
 			stmt.executeUpdate(sql);
-			System.out.println("Inserted intervention into Intervention table.");
+			System.out.println("Inserted AI intervention into Intervention table.");
 			
 			stmt.close();
 			conn.close();
